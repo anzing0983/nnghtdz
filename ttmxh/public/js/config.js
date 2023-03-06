d1984d7c3989c965ab54c91dedb8d49481b2cd65
Nguyễn Ngọc Huy Tâm <108131520+anzing0983@users.noreply.github.com>
2023-03-06 12:48:17 +0700

    Add files via upload

---
+++ ttmxh/public/js/config.js
@@ -0,0 +1,59 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'Nguyen Ngoc Huy Tam',
+        'favicon' : 'https://scontent.xx.fbcdn.net/v/t1.15752-9/334918180_1156237638422386_2751330646472111_n.png?_nc_cat=100&ccb=1-7&_nc_sid=aee45a&_nc_ohc=WR6m0uDGCfwAX8yKUR_&_nc_oc=AQkK88M6lhWWopyVCmeZYL3BrZ9WkLEdO5zN7nON3KxGdxD_GqL6X_qOp5Gpr2xG24b_bEqG_roOS2llMy9Z2mkf&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQaE393RZYO5_UmsDU25a92JZ3km2fPFqVYbKbleJ_qjw&oe=642CD5EE',
+        'notice': 'Nguyen Ngoc Huy Tam DTRAI S1'
+    },
+    'avatar-image' : 'https://scontent.xx.fbcdn.net/v/t1.15752-9/334918180_1156237638422386_2751330646472111_n.png?_nc_cat=100&ccb=1-7&_nc_sid=aee45a&_nc_ohc=WR6m0uDGCfwAX8yKUR_&_nc_oc=AQkK88M6lhWWopyVCmeZYL3BrZ9WkLEdO5zN7nON3KxGdxD_GqL6X_qOp5Gpr2xG24b_bEqG_roOS2llMy9Z2mkf&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQaE393RZYO5_UmsDU25a92JZ3km2fPFqVYbKbleJ_qjw&oe=642CD5EE',
+    'name' : 'Nguyễn Ngọc Huy Tâm ', 
+    'is_tick' : true,
+    'skills' : [
+        'Student ',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'https://scontent.xx.fbcdn.net/v/t1.15752-9/334918180_1156237638422386_2751330646472111_n.png?_nc_cat=100&ccb=1-7&_nc_sid=aee45a&_nc_ohc=WR6m0uDGCfwAX8yKUR_&_nc_oc=AQkK88M6lhWWopyVCmeZYL3BrZ9WkLEdO5zN7nON3KxGdxD_GqL6X_qOp5Gpr2xG24b_bEqG_roOS2llMy9Z2mkf&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQaE393RZYO5_UmsDU25a92JZ3km2fPFqVYbKbleJ_qjw&oe=642CD5EE',
+            'content' : 'CONTACT ADMIN',
+            'author' : 'Nguyễn Ngọc Huy Tâm',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'wallet.png',
+            'title' : 'Thông Tin Thanh Toán', 
+            'show-value' : 'Nguyễn Ngọc Huy Tâm',
+            'value' : 'https://nnht-bank.blogspot.com/',
+            'type' : 0,
+        },
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'Facebook', 
+            'show-value' : '@NguyenNgocHuyTam.Info' ,
+            'value' : 'https://www.facebook.com/NguyenNgocHuyTamxDtlacuti//',
+            'type' : 0,
+        },
+        {
+            'image' : 'zalo.jpg',
+            'title' : 'Zalo', 
+            'show-value' : 'Nguyễn Ngọc Huy Tâm',
+            'value' : 'https://zalo.me/0777522806',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Hotline', 
+            'show-value' : 'xxxx xxx xxx',
+            'value' : 'tel:xxxx xxx xxx',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail', 
+            'show-value' : 'huytam2k7@gmail.com',
+            'value' : 'mailto:huytam2k7@gmail.com',
+            'type' : 0,
+        },
+    ]
+};
+
